33b61e9c516a9089a448157c722a76c3e100ad0b
Netodev3 <105382120+Netodev3@users.noreply.github.com>
2023-11-29 15:48:20 +0000

    Generador de Excusas con Boton

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function randExcuse() {
+    let who = ["Mi perro", "Mi abuela", "Su tortuga", "Mi pájaro"];
+    let action = ["se comió", "meó", "aplastó", "rompió"];
+    let what = ["mis deberes", "las llaves", "el coche"];
+    let when = [
+      "antes de las clases",
+      "justo a tiempo",
+      "cuando acabé",
+      "durante mi almuerzo",
+      "cuando estaba rezando"
+    ];
+
+    const whoRand = who[Math.floor(Math.random() * who.length)];
+    const actionRand = action[Math.floor(Math.random() * action.length)];
+    const whatRand = what[Math.floor(Math.random() * what.length)];
+    const whenRand = when[Math.floor(Math.random() * when.length)];
+
+    let excusaRandom = `${whoRand} ${actionRand} ${whatRand} ${whenRand}`;
+
+    return excusaRandom;
+  }
+
+  document.querySelector("#excuse-button").addEventListener("click", () => {
+    document.getElementById("excuse").innerHTML = randExcuse();
+  });
 };
